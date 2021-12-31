2b7c9e534e6d98624c01f28ba9203c423ef1f361
Sam Selikoff <sam.selikoff@gmail.com>
2021-12-31 10:12:15 -0500

    working!

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
+import { Suspense } from "react";
 import ChipotleStats from "../components/chipotle-stats";
+import Spinner from "../components/spinner";
 import TwitterStats from "../components/twitter-stats";
 import YouTubeStats from "../components/youtube-stats";
 
@@ -7,11 +9,21 @@ export default function Home() {
     <div className="p-8">
       <h3 className="text-lg font-medium leading-6 text-gray-900">Dashboard</h3>
 
-      <div className="grid grid-cols-3 gap-5 mt-5">
-        <TwitterStats />
-        <YouTubeStats />
-        <ChipotleStats />
-      </div>
+      <Suspense fallback={<Fallback />}>
+        <div className="grid grid-cols-3 gap-5 mt-5">
+          <TwitterStats />
+          <YouTubeStats />
+          <ChipotleStats />
+        </div>
+      </Suspense>
+    </div>
+  );
+}
+
+function Fallback() {
+  return (
+    <div className="h-[100px] flex items-center justify-center mt-5">
+      <Spinner />
     </div>
   );
 }
